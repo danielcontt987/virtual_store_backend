3e259b87cbc92f14c1890c50cfd59d0d6b52b85a
danielcontt987 <josedanielcontrerasperez510@gmail.com>
2025-11-30 21:55:05 -0600

    Agregando las validaciones en el dto

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,19 @@
-export class CreateProductDto {}
+import { IsInt, IsNotEmpty, IsNumber, IsString } from "class-validator"
+
+export class CreateProductDto {
+    @IsNotEmpty({message: "El nombre no puede ser vacio"})
+    @IsString({message: "Valor no valido"})
+    name: string
+
+    @IsNotEmpty({message: "El precio no puede estar vacio"})
+    @IsNumber({maxDecimalPlaces: 2 }, {message: 'Solo dos decimales'})
+    price: number
+
+    @IsNotEmpty({message: "El inventario no puede estar vacio"})
+    @IsNumber({maxDecimalPlaces: 0 }, {message: 'Cantidad no valida'})
+    inventory: number
+
+    @IsNotEmpty({message: "La categoría no puede estar vacia"})
+    @IsInt({message: "La categoría no es valida"})
+    category_id: number
+}
